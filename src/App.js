0e2830ac44be28159575bee922c44e488ec368b2
Kevin Castillo <kevincastillo318@gmail.com>
2022-11-04 15:29:03 -0700

    sidebar made

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ import Signup from "./pages/Signup";
 import Create from "./pages/Create";
 import Project from "./pages/Project";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return <div className="App">
-    <BrowserRouter>
-      <div className="container">
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-          <Route path="/create">
-            <Create />
-          </Route>
-          <Route path="/projects/:id">
-            <Project />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
