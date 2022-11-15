3de54ddba5b31a8e9fca94166b2722c201286599
Kevin Castillo <kevincastillo318@gmail.com>
2022-11-14 17:02:27 -0800

    conditional navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import "./Navbar.css";
+import { useAuthContext } from "../hooks/useAuthContext";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import Cartoon from "../assets/cartoon.svg";
 
 export default function Navbar() {
+  const { user } = useAuthContext();
   const { logout, isPending } = useLogout();
 
   return (
@@ -13,24 +15,32 @@ export default function Navbar() {
           <img src={Cartoon} alt="dogo logo" />
           <span>Frankenmanage</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Logging out
-            </button>
-          )}
-        </li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging out
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   );
