f1cfc64b3c8e7c139cb0effd1f9b3fc1c435c8d4
Kevin Castillo <kevincastillo318@gmail.com>
2022-11-27 11:27:54 -0800

    dashboard shows projects

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,14 @@
-import "./Dashboard.css"
+import "./Dashboard.css";
+import { useCollection } from "../hooks/useCollection";
 
 export default function Dashboard() {
+  const { documents, error } = useCollection("projects");
+
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && documents.map((doc) => <p key={doc.id}>{doc.name}</p>)}
+    </div>
+  );
 }
