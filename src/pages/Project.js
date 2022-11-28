8c0e7e2c2fd265039d6ee32adc5e006136370ecb
Kevin Castillo <kevincastillo318@gmail.com>
2022-11-28 14:48:27 -0800

    project details code

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,22 @@
-import "./Project.css"
+import { useParams } from "react-router-dom";
+import { useDocument } from "../hooks/useDocument";
+import "./Project.css";
 
 export default function Project() {
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
   return (
-    <div>Project</div>
-  )
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
