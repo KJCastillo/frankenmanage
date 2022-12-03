d317351d7d1debd6a987db0342b4d91815c465b8
Kevin Castillo <kevincastillo318@gmail.com>
2022-12-03 14:36:46 -0800

    filter css

--- src/pages/dashboard/ProjectFitlers.js
+++ src/pages/dashboard/ProjectFitlers.js
@@ -1,29 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const filterList = ['all', 'mine', 'development', 'design', 'marketing', 'sales']
+const filterList = [
+  "all",
+  "mine",
+  "development",
+  "design",
+  "marketing",
+  "sales",
+];
 
 export default function ProjectFitlers() {
-    const [currentFilter, setCurrentFitler] = useState('all')
+  const [currentFilter, setCurrentFitler] = useState("all");
 
-    const handleClick = (newFilter) => {
-        console.log(newFilter)
-        setCurrentFitler(newFilter)
-    }
+  const handleClick = (newFilter) => {
+    console.log(newFilter);
+    setCurrentFitler(newFilter);
+  };
 
   return (
-    <div className='project-filter'>
-        <nav>
+    <div className="project-filter">
+      <nav>
+        <p>Filter by:</p>
         {filterList.map((f) => (
-            <button key={f}
-                onClick={() => handleClick(f)}
-                className={currentFilter === f ? 'active' : ''} 
-                //dynamic class name - check if current filter is active
-                //if current filter matches f, then class name will be 'active'
-            >
-                {f}
-            </button>
+          <button
+            key={f}
+            onClick={() => handleClick(f)}
+            className={currentFilter === f ? "active" : ""}
+            //dynamic class name - check if current filter is active
+            //if current filter matches f, then class name will be 'active'
+          >
+            {f}
+          </button>
         ))}
-        </nav>
+      </nav>
     </div>
-  )
+  );
 }
